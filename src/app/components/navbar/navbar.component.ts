ed008b2dd59965b6f72ff587f36adaf65b413071
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-04-16 10:03:59 -0400

    updates

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'bt-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  paths = [
+    { path: '/home', name: 'Home' },
+    { path: '/contact', name: 'Contact' },
+    { path: '/questions', name: 'FAQ' },
+    { path: '/about', name: 'About' },
+  ]
 }
