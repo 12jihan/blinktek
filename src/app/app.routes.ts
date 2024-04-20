9e4ccd26e1e19cbfacad837cd496399bd4eb6719
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-04-20 12:32:24 -0400

    made more changes to the site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 export const routes: Routes = [
-    { path: 'landing', component: LandingPageComponent },
     { path: '', pathMatch: 'full', redirectTo: '/landing'},
+    { path: 'landing', component: LandingPageComponent },
 ];
