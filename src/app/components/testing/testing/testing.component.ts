a36c63d4e7f7b562cebd25322f566be1fb9e84ed
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-08-31 18:41:13 -0400

    made some hard changes

---
+++ src/app/components/testing/testing/testing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bt-testing',
+  standalone: true,
+  imports: [],
+  templateUrl: './testing.component.html',
+  styleUrl: './testing.component.scss'
+})
+export class TestingComponent {
+
+}
