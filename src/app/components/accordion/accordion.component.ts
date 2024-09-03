a861e4aa342756f56a0b785896a794bf51e23263
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-09-03 16:44:42 -0400

    added angular module for this too

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { faFortAwesome } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'bt-accordion',
   standalone: true,
-  imports: [],
+  imports: [
+
+  ],
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.scss'
 })
