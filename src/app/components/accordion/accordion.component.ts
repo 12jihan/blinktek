152a2f8d6de99e22bd4fb8187bd8b8936bbd675a
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-09-04 04:49:15 -0400

    updated more stuff i guess but it's currently not working -- accordion component

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-import { faFortAwesome } from '@fortawesome/free-brands-svg-icons';
+import { Component, ViewChild, ViewChildren } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFortAwesome, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'bt-accordion',
   standalone: true,
   imports: [
-
+    FontAwesomeModule
   ],
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.scss'
 })
 export class AccordionComponent {
+  @ViewChild('container', { static: true }) content: ViewChildren | undefined = undefined;
+  chevron_down: IconDefinition = faChevronDown;
+  opened: boolean = false;
 
+  public open_close(): void {
+    this.opened = !this.opened;
+  }
 }
